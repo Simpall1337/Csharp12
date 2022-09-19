19da1be0cfe3070ecfdbca513912cf30f3cbcb90
Alex Huzo <sanduguzo1337@gmail.com>
2022-09-19 13:24:48 +0300

    12

---
+++ 12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Lessons
+{
+    internal class Program
+    {
+        static void SumArray(int[] myArray, ref int sum, int i = 0)
+        {
+            if (i == myArray.Length)
+                return;
+            sum += myArray[i];
+            i++;
+            SumArray(myArray, ref sum, i);
+        }
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int[] myArray = new int[5] { 1, 2, 3, 4, 5 };
+            SumArray(myArray, ref sum);
+            Console.WriteLine(sum);
+        }
+    }
+}
